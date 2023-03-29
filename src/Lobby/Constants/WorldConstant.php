8ef8579486ecebcf11d4166ea70883887c243d2e
Fanouu <MileniumShad@gmail.com>
2023-03-29 13:17:48 +0200

    test

---
+++ src/Lobby/Constants/WorldConstant.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Lobby\Constants;
+
+class WorldConstant{
+
+    const WORLD_LOBBY = "lobby";
+    const WORLD_LOBBY2 = "Lobby2";
+}
---
+++ src/Lobby/Entities/TitleEntity.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Lobby\Entities;
+
+use Lobby\Main;
+use pocketmine\entity\Human;
+use pocketmine\entity\Location;
+use pocketmine\entity\Skin;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\player\Player;
+
+class TitleEntity extends Human {
+
+    private ?string $npc_type = null;
+
+    public function __construct(Location $location, Skin $skin, CompoundTag $nbt = null) {
+        parent::__construct($location, $skin, $nbt);
+        $this->setNameTagAlwaysVisible(false);
+        $this->gravityEnabled = false;
+        $this->setScale(3.5);
+        $this->setImmobile();
+    }
+
+    protected function initEntity(CompoundTag $nbt) : void {
+        parent::initEntity($nbt);
+        $this->setNameTagAlwaysVisible(false);
+        $this->gravityEnabled = false;
+        $this->setScale(3.5);
+        $this->setImmobile();
+    }
+
+    public function attack(EntityDamageEvent $source) : void {
+        if($source instanceof EntityDamageByEntityEvent){
+            $player = $source->getDamager();
+            if($player instanceof Player){
+                if($player->getInventory()->getItemInHand()->getId() === 511){
+                    $this->kill();
+                    $this->flagForDespawn();
+                    $this->close();
+                    return;
+                }
+            }
+        }
+        $source->cancel();
+    }
+
+    public static function getSkinT() : Skin{
+        $data = self::PNGtoBYTES(Main::getInstance()->getDataFolder() . "texture.png");
+        $geometry = file_get_contents(Main::getInstance()->getDataFolder() . "endiorite_title.geo.json");
+        return new Skin("Title", $data, "","geometry.endiorite_title", $geometry);
+    }
+
+    public static function PNGtoBYTES($path) : string{
+        $img = @imagecreatefrompng($path);
+        $bytes = "";
+        for ($y = 0; $y < (int) @getimagesize($path)[1]; $y++) {
+            for ($x = 0; $x < (int) @getimagesize($path)[0]; $x++) {
+                $rgba = @imagecolorat($img, $x, $y);
+                $bytes .= chr(($rgba >> 16) & 0xff) . chr(($rgba >> 8) & 0xff) . chr($rgba & 0xff) . chr(((~(($rgba >> 24))) << 1) & 0xff);
+            }
+        }
+        @imagedestroy($img);
+        return $bytes;
+    }
+
+}
